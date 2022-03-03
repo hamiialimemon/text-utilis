d22c7043265c2baf3e0856f89c1e9696f9582977
hamiialimemon <hamidalimemon47@gmail.com>
2022-03-03 23:11:04 +0500

    initial commit

--- textutils/settings.py
+++ textutils/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r!gx0jzijzh=0e26p#@x530_r#yhi!f-p$$a7#6v)9*%cgly0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
